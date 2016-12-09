d20816bb24a8bda47ee8223e0dcc652777076e8f
komolov-andrey <komolov.1995@gmail.com>
2016-12-09 23:22:33 +0300

    add switching between items in client

--- Documents/NetBeansProjects/ias/src/java/dataBean/Client.java
+++ Documents/NetBeansProjects/ias/src/java/dataBean/Client.java
@@ -6,26 +6,43 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
-@ManagedBean(name = "clientText")
+@ManagedBean(name = "client")
 public class Client implements Serializable {
-    private static boolean show = true;
+    private static boolean showUsl = true;    
+    private static boolean showCost = false;
 
-    public static void setShow(boolean show) {
-        Client.show = show;
+    public static void setShowUsl(boolean show) {
+        Client.showUsl = show;
+    }
+//тут дальше перебор что выключать что включать в отображение
+    public boolean isShowUsl() {
+        return showUsl;
+    }
+    
+    public void showUsl() {
+        setShowUsl(true);
+        setShowCost(false);
+        //FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, isShow() + "", "test");
+        //FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    public void showNotUsl() {
+        setShowUsl(false);
+    }
+    
+    public static void setShowCost(boolean show) {
+        Client.showCost = show;
+    }
+//тут дальше перебор что выключать что включать в отображение
+    public boolean isShowCost() {
+        return showCost;
     }
 
-    public boolean isShow() {
-        return show;
+    public void showCost() {
+        setShowCost(true);
+        setShowUsl(false);
     }
-
-    public void changeshow() {
-        if (isShow() == false) {
-            setShow(true);
-        } else {
-            setShow(false); 
-        }
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, isShow() + "", "test");
-        FacesContext.getCurrentInstance().addMessage(null, message);
+    public void showNotCost() {
+        setShowUsl(false);
     }
 
 }
