a2a57c296175c07db5823cbb7a0b2787efbc82b6
komolov-andrey <komolov.1995@gmail.com>
2016-12-13 20:25:35 +0300

    add date format

--- Documents/NetBeansProjects/ias/src/java/dataBean/Record.java
+++ Documents/NetBeansProjects/ias/src/java/dataBean/Record.java
@@ -6,6 +6,8 @@
 package dataBean;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import javax.faces.application.FacesMessage;
@@ -21,7 +23,7 @@ import org.primefaces.event.FlowEvent;
 @ManagedBean
 @SessionScoped
 public class Record implements Serializable {
-    
+
     private ArrayList<String> citys;
     private String city;
     private ArrayList<String> hospitals;
@@ -33,13 +35,36 @@ public class Record implements Serializable {
     //подумать
     private Date date;
     private Date time;
+    private String dateString;
+    private String timeString;
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    public void setDateString(String dateString) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String s = df.format(date);
+        this.dateString = s;
+    }
+
+    public String getTimeString() {
+        return timeString;
+    }
+
+    public void setTimeString(String timeString) {
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        String s = df.format(time);
+        this.timeString = s;
+    }
 
     public Date getTime() {
         return time;
     }
 
     public void setTime(Date time) {
-        this.time = time;
+        this.time = time;        
+        setTimeString(time.toString());
     }
 
     public Date getDate() {
@@ -48,6 +73,7 @@ public class Record implements Serializable {
 
     public void setDate(Date date) {
         this.date = date;
+        setDateString(date.toString());
     }
 
     public String getDoctor() {
@@ -74,7 +100,7 @@ public class Record implements Serializable {
         this.hospital = hospital;
     }
 
-    public void setCity(String city ) {
+    public void setCity(String city) {
         this.city = city;
     }
 
@@ -83,37 +109,37 @@ public class Record implements Serializable {
     }
 
     public ArrayList<String> getCitys() {
-        
+
         citys = new ArrayList<String>();
-        
+
         db.DataConn db = new db.DataConn();
         db.qeuryRequest("select city from hospitals;");
         citys = db.queryField("city");
         db.closeConn();
-        
+
         return citys;
     }
-    
+
     public ArrayList<String> getHospitals() {
-        
+
         db.DataConn db = new db.DataConn();
         db.qeuryRequest("select name from hospitals where city = '" + city + "';");
         hospitals = db.queryField("name");
         db.closeConn();
-        
+
         return hospitals;
     }
-    
+
     public ArrayList<String> getCats() {
-        
+
         cats = new ArrayList<String>();
         cats.add("imynolog");
         cats.add("dantist");
         return cats;
     }
-    
+
     public ArrayList<String> getDoctors() {
-        
+
         doctors = new ArrayList<String>();
         doctors.add("ivanov");
         doctors.add("petrov");
