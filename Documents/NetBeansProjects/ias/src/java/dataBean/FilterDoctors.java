51200fe343624ae40a6e8fed53a4fd950281f970
komolov-andrey <komolov.1995@gmail.com>
2016-12-26 12:58:34 +0300

    add in table data from db

--- Documents/NetBeansProjects/ias/src/java/dataBean/FilterDoctors.java
+++ Documents/NetBeansProjects/ias/src/java/dataBean/FilterDoctors.java
@@ -16,21 +16,54 @@ import list.DocItem;
 
 @ManagedBean(name = "filterViewDoctors")
 @ViewScoped
-public class FilterDoctors implements Serializable { 
+public class FilterDoctors implements Serializable {
 
     private List<DocItem> doctors;
-    private List<DocItem> filteredDoctors;     
+    private List<DocItem> filteredDoctors;
 
     @PostConstruct
     public void init() {
-        doctors = createDoctors(10);
+        doctors = createDoctors();
     }
-     
-    public List<DocItem> createDoctors(int size) {
+
+    public List<DocItem> createDoctors() {
         List<DocItem> list = new ArrayList<DocItem>();
         //add from db
-            list.add(new DocItem("кат1", "фам1", "им1"));
-            list.add(new DocItem("кат2", "фам2", "им2"));
+        db.DataConn db = new db.DataConn();
+        db.qeuryRequest("select id_hosp from Regs where @rid = '" + User.getId_regs() + "';");
+
+        ArrayList id_hosp = db.queryField("id_hosp");
+        ArrayList categoryName = new ArrayList();
+        ArrayList fam = new ArrayList();
+        ArrayList im = new ArrayList();
+        if (id_hosp.size() > 0) {
+            //удалить []
+            String id_hospital = id_hosp.get(0).toString();
+            id_hospital = id_hospital.substring(1, id_hospital.length() - 1);
+
+            db.qeuryRequest("select id_category, fam, im from Doctors where id_hosp = " + id_hospital + ";");
+
+            try {
+                ArrayList category = db.queryField("id_category");
+                for (int i = 0; i < category.size(); i++) {
+                    String id_cat = category.get(i).toString();
+                    id_cat = id_cat.substring(1, id_cat.length() - 1);
+                    db.qeuryRequest("select from Cat_doc where @rid = " + id_cat + ";");
+                    categoryName.add(db.queryField("name").get(0).toString());
+                }
+            db.qeuryRequest("select id_category, fam, im from Doctors where id_hosp = " + id_hospital + ";");
+                fam = db.queryField("fam");
+                im = db.queryField("im");
+                for (int i = 0; i < fam.size(); i++) {
+                    list.add(new DocItem(categoryName.get(i).toString(), fam.get(i).toString(), im.get(i).toString()));
+                }
+            } catch (Exception e) {
+
+            } finally {
+                db.closeConn();
+            }
+        }
+
         return list;
     }
 
